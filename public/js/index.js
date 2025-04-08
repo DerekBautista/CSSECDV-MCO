ea003d91349b245863209686bfcfbc7a59ad1014
Derek Bautista <derek_roi_t_bautista@dlsu.edu.ph>
2025-04-08 15:16:35 +0800

    Revert "Update index.js"
    This reverts commit 1626387607e3ad82c443d30000f1db1f46271b26.

--- public/js/index.js
+++ public/js/index.js
@@ -107,26 +107,22 @@ $("#login-submit-btn").on('click', async function (event) {
         }
         else{
             // Check if the password is correct
-            try{
-                const passwordResponse = await fetch(`/user/isPassword?companyID=${companyID}&password=${loginPassword}`, {
-                    method: 'GET'
-                });
-    
-                const passwordData = await passwordResponse.json()
-    
-                if(!passwordData.authenticated){
-                    if(passwordData.remainingAttempts > 0)
-                        $("#login-error-message").html('Failed Login. (' + passwordData.remainingAttempts + ' attempts remaining.)').css('display', 'block');
-                    else{
-                        const lockedUntil = new Date(passwordData.lockedUntil);
-                        setLockoutTimer(lockedUntil)
-                    }
-                }
+            const passwordResponse = await fetch(`/user/isPassword?companyID=${companyID}&password=${loginPassword}`, {
+                method: 'GET'
+            });
+
+            const passwordData = await passwordResponse.json()
+
+            if(!passwordData.authenticated){
+                if(passwordData.remainingAttempts > 0)
+                    $("#login-error-message").html('Failed Login. (' + passwordData.remainingAttempts + ' attempts remaining.)').css('display', 'block');
                 else{
-                    $("#login-form").submit();
+                    const lockedUntil = new Date(passwordData.lockedUntil);
+                    setLockoutTimer(lockedUntil)
                 }
-            }catch (err) {
-                console.log("Network error (e.g., server down):", err);
+            }
+            else{
+                $("#login-form").submit();
             }
         }
     } catch (err) {
